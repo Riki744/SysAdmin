58e5dec66a5f0fcd7a41918839daae79d731647f
Riki744 <85706972+Riki744@users.noreply.github.com>
2021-11-22 13:24:25 +0200

    Add files via upload
    Powershell script that will help you to automate process with SCCM package to delete files on specific folders in Windows System

---
+++ Remove_application.ps1
@@ -0,0 +1,24 @@
+﻿#Deletes file from provided location, if file not existing 
+
+$file = 'path to file'
+$log = Start-Transcript -Path "C:\Windows\temp\log_name.txt"
+
+#If file exists delete it, if does not do nothing
+if (Test-Path -Path $file -PathType Leaf) {
+    try {
+        $removefile = Remove-Item -Path $file -Force -ErrorAction Stop -Verbose
+        Write-Host "The file [$file] has been deleted"
+
+    }
+    catch {
+        Write-Warning -Message "[$file] not removed"
+
+    }
+}
+#if the file not existing, show message do nothing
+else {
+    Write-Host "File already deleted"
+}
+$log_s = Stop-Transcript
+    
+
